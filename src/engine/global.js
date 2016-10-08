08f2bcdc1b6f45d91f5db64a94a8337336fda880
ArthurYV <yangjingbing007@gmail.com>
2016-10-08 19:40:22 +0800

    添加 global.js

---
+++ src/engine/global.js
@@ -0,0 +1,13 @@
+export default {
+
+    KEY_ESC: 27,
+    KEY_ENTER: 13,
+    KEY_CHAT: 13,
+    KEY_DEFEND: 69,
+    KEY_LEFT: 37,
+    KEY_UP: 38,
+    KEY_RIGHT: 39,
+    KEY_DOWN: 40,
+    KEY_BIGBOOM: 81,
+
+}
