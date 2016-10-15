fc4b2997c6dfb92da6a17bd3b06e6c85dc2e7071
lcj <luchenjiemail@gmail.com>
2016-10-15 14:16:08 +0800

    almost finish the login function

--- src/socket/analyis.js
+++ src/socket/analyis.js
@@ -11,10 +11,50 @@
 // 			body : ""
 // 			}
 
+//reverse a string
+function reverseString(str){
+	return str.split("").reverse().join("");
+}
+
+//fill information to DataView
+function fillDw(dw,content,end){
+	content = reverseString(content);
+	let length = content.length;
+	while(length--){
+		dx.setUint8(end--,content[length]);
+	}
+}
+
+//fill connection information to DataView
+function fillConnectFoeDw(dw,body){
+	let userId = body.id.toString(2);
+	let lengthOfName = body.nickname.lengthOfName.toString(2);
+	let name=body.nickname.name.toString(2);
+	let roomNumber = body.roomNumber.toString(2);
+	let troop = body.troop.toString(2);
+	let nameEnd = 175+body.nickname.lengthOfName*8;
+	fillDw(dw,userId,167);
+	fillDw(dw,lengthOfName,175);
+	fillDw(dw,name,nameEnd);
+	fillDw(dw,roomNumber,nameEnd+32);
+	fillDw(dw,troop,nameEnd+40);
+}
+
 //transfrom message to binary
 function toBinary(message){
-	dw = new DataView(message.length);
-	
+	let buffer = new ArrayBuffer(message.length);
+	let dw = new DataView(buffer);
+	let length = message.length.toString(2);
+	let timestamp = message.timestamp.toString(2);
+	let type = message.type.toString(2);
+	fillDw(dw,length,31);
+	fillDw(dw,timestamp,95);
+	fillDw(dw,type,103);
+	switch( type ){
+		case "1001" :
+			fillConnectForDw(dw,message.body);
+			break;
+	}
 }
 
 //analyis the information of login
--- src/socket/transmitted.js
+++ src/socket/transmitted.js
@@ -6,11 +6,12 @@
 
 import ws from "./websocket.js";
 import gamemodel from "../model/gamemodel.js" 
-module analyis from "./analyis.js"
+import * as analyis from "./analyis.js"
 
 export default class transmitted{
 	//send login message
 	login(airplane){ 
 		let message = analyis.loginAnalyis(airplane);
+		console.log(message);
 	}
 }
