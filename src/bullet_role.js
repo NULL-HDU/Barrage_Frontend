e1c6425c679e27b83d0c7b1004c7a14ea085afe3
mpsss <mephistommm@gmail.com>
2016-11-23 18:43:58 +0800

    create circle path bullet

--- src/bullet_role.js
+++ src/bullet_role.js
@@ -1,13 +1,13 @@
 
 import global from "./global.js";
-import {straightLinePath} from "./pathFunc.js";
+import {testPath} from "./pathFunc.js";
 
 export const STRAIGHT_LINE_BULLET = 0;
 
 export default {
   [STRAIGHT_LINE_BULLET]: {
     radius: 5,
-    pathFunc: straightLinePath,
+    pathFunc: testPath,
     hp: 100,
     damage: 50,
     speed: global.BULLET_SPEED,
--- src/pathFunc.js
+++ src/pathFunc.js
@@ -1,6 +1,6 @@
 import PVector from "./model/Point.js";
 
-export const straightLinePath = (bullet) => {
+const straightLinePath = (bullet) => {
   let angle = bullet.attackDir % (2 * Math.PI);
   let sv = new PVector(
     Math.cos(angle + (3/2)*Math.PI) * bullet.speed,
@@ -11,3 +11,26 @@ export const straightLinePath = (bullet) => {
     bullet.locationCurrent.add(sv);
   };
 };
+
+const circlePath = (bullet) => {
+  let angle = bullet.attackDir % (2 * Math.PI);
+  let sv = new PVector(
+    Math.sin(angle + (3/2)*Math.PI) * bullet.speed,
+    - Math.cos(angle + (3/2)*Math.PI) * bullet.speed
+  );
+  let R = 50; // a = v**2 / R
+  let dR = 0.2;
+
+  return () => {
+    let asv = new PVector(sv.y , -sv.x);
+    asv.setMag(Math.pow(bullet.speed, 2) / R);
+    sv.add(asv);
+    sv.setMag(bullet.speed);
+    if(R <= 400){
+      R += dR;
+    }
+    bullet.locationCurrent.add(sv);
+  };
+};
+
+export const testPath = circlePath;
