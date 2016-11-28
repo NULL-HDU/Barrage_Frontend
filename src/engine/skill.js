841d442ea29b9a98eb8ed7d819b5cec06eab52c0
ArthurYV <yangjingbing007@gmail.com>
2016-11-28 15:26:22 +0800

    modify

--- src/engine/skill.js
+++ src/engine/skill.js
@@ -38,6 +38,8 @@ let normalSkillFunc = () => {
     let airPlane = data.airPlane;
     let angle = airPlane.attackDir;
     let bullet = new Bullet(airPlane.userId, STRAIGHT_LINE_BULLET, angle);
+    console.log("bullet is ");
+    console.log(bullet);
     var count = new Count();
     bullet.id = count.getLastCount();
     gamemodel.socketCache.newBallInformation.push(bullet);
