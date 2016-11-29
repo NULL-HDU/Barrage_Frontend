c4056443d47178c86102910e83c535fa0f69f834
mpsss <mephistommm@gmail.com>
2016-11-29 16:48:55 +0800

    move KEY_* to constant.KEY

--- src/constant.js
+++ src/constant.js
@@ -9,3 +9,23 @@ export const FOOD = 3;
 export const ALIVE = 0;
 export const DEAD = 1;
 export const DISAPPEAR = 2;
+
+// key code
+export const KEY = {
+  ESC: 27,
+  ENTER: 13,
+  CHAT: 13,
+  DEFEND: 69,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  BIGBOOM: 81,
+  SPACE: 32,
+  SHIFT: 16,
+  W: 87,
+  A: 65,
+  S: 83,
+  D: 68,
+  E: 69,
+};
--- src/global.js
+++ src/global.js
@@ -1,23 +1,4 @@
 export default {
-
-    KEY_ESC: 27,
-    KEY_ENTER: 13,
-    KEY_CHAT: 13,
-    KEY_DEFEND: 69,
-    KEY_LEFT: 37,
-    KEY_UP: 38,
-    KEY_RIGHT: 39,
-    KEY_DOWN: 40,
-    KEY_BIGBOOM: 81,
-
-    KEY_SPACE: 32,
-    KET_SHIFT: 16,
-    KEY_W: 87,
-    KEY_A: 65,
-    KEY_S: 83,
-    KEY_D: 68,
-    KEY_E: 69,
-
     // skill
     NORMAL_SKILL: 0,
     E_SKILL: 1,
@@ -26,17 +7,16 @@ export default {
     E_SKILL_CD: 1000,
     SKILL_CHECk_LOOP_INTERVAL: 50,
 
-    NORMAL_SKILL: 0,
-    NORMAL_SKILL_CD: 500,
-    SKILL_CHECk_LOOP_INTERVAL: 50,
-
+    // quadtree
     QUAD_BALL_AMOUNT: 4,
     QUAD_TREE_DEPTH: 30,
 
+    // airPlane and bullet
     AIRPLANE_SPEED: 2.5,
     AIRPLANE_SLOW_RATE: 0.3,
     BULLET_SPEED: 200,              // px / s
 
+    // loops
     BULLET_MAKER_LOOP_INTERVAL: (1/5)*1000,
     BULLET_MAKER_DEMO_LOOP_INTERVAL: (1/10)*1000,
     BULLET_COLLECTING_INTERVAL: ( 1/30)*1000,
@@ -46,4 +26,4 @@ export default {
     LOCAL_HEIGHT: window.screen.height,
     LOCAL_WIDTH: window.screen.width,
 
-}
+};
