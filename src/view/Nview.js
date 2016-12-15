21405423dec75cda2d4df5ea8cec6695e1461e1a
Neoco <Neoco.wlp1002@gmail.com>
2016-12-15 19:05:22 +0800

    export looprender

--- src/view/Nview.js
+++ src/view/Nview.js
@@ -340,14 +340,13 @@ function initUI() {
 }
 
 // loop render
-function loopRender() {
+export function loopRender() {
     state();
     rszView();
     renderer.render(Stage);
     GMD.deadCache = [];
     GMD.disappearCache = [];
     GMD.collisionCache = [];
-    setTimeout(loopRender, stot);
 }
 
 function play() {
