dba6dd5cd1445f094c423b1e5e4e7b6b63512057
mpsss <mephistommm@gmail.com>
2016-12-20 11:56:52 +0800

    optimize collision detection; add new bullet; add codes for info cancel

--- src/resource/bullet/roleId.js
+++ src/resource/bullet/roleId.js
@@ -4,7 +4,16 @@
  * Email: mephistommm@gmail.com
  */
 
-export const STRAIGHT_LINE_BULLET = 0;
-export const CIRCLE_BULLET = 1;
+export const MIN_STRAIGHT_LINE_BULLET = 0;
+export const STRAIGHT_LINE_BULLET = 1;
+export const MAX_STRAIGHT_LINE_BULLET = 2;
+
+export const MIN_CIRCLE_BULLET = 3;
+export const CIRCLE_BULLET = 4;
+export const MAX_CIRCLE_BULLET = 5;
+
+export const MIN_UNIFORMLY_RETARDED_BULLET = 6;
+export const UNIFORMLY_RETARDED_BULLET = 7;
+export const MAX_UNIFORMLY_RETARDED_BULLET = 8;
 
 /* roleId of bullet ends here */
